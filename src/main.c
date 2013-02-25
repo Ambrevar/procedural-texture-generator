45b7b0b636a9040480a6688750196128e94965f7
Pierre Neidhardt <ambrevar@gmail.com>
2013-02-25 11:57:38 +0100

    Interporl optimization.

--- src/main.c
+++ src/main.c
@@ -224,45 +224,40 @@ interpol (long y1, long y2, long step, long delta)
     */
 }
 
-
-// TODO: refactor.
 long
 interpol_val (long i, long j, long frequency, layer * current_layer)
 {
     // Bound values.
     long bound1i, bound1j, bound2i, bound2j;
-    long buf;
 
-    // TODO: no need for doubles in all this function.
-    double step = (double) current_layer->size / frequency;
+    long step = current_layer->size / frequency;
+    if (step == 0)
+        return current_layer->v[i][j];
 
-    buf = (double) i / step;
-    bound1i = buf * step;
+    bound1i = i / step * step;
     bound2i = bound1i + step;
 
     if (bound2i >= current_layer->size)
         bound2i = current_layer->size - 1;
 
-    buf = (double) j / step;
-    bound1j = buf * step;
+    bound1j = j / step * step;
     bound2j = bound1j + step;
 
     if (bound2j >= current_layer->size)
         bound2j = current_layer->size - 1;
 
-    int b11, b12, b21, b22;
+    long b11, b12, b21, b22;
     b11 = current_layer->v[bound1i][bound1j];
     b12 = current_layer->v[bound1i][bound2j];
     b21 = current_layer->v[bound2i][bound1j];
     b22 = current_layer->v[bound2i][bound2j];
 
-    int v1 = interpol (b11, b12, step, j - bound1j);
-    int v2 = interpol (b21, b22, step, j - bound1j);
-    int result = interpol (v1, v2, step, i - bound1i);
+    long v1 = interpol (b11, b12, step, j - bound1j);
+    long v2 = interpol (b21, b22, step, j - bound1j);
+    long result = interpol (v1, v2, step, i - bound1i);
 
     return result;
 }
-
 
 layer *
 init_layer (long size)
