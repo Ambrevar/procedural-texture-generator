e66ea2693724728f31518090d6a55eafdbf6b3ea
Pierre Neidhardt <ambrevar@gmail.com>
2013-02-25 22:21:49 +0100

    Test fix.

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ../src/procedural-textures ../data/wood.ptx && ls -l *bmp && \
-    sha1sum res_new/result_RGB.bmp result_RGB.bmp && \
-    sha1sum res_new/result_RGB_smooth.bmp result_RGB_smooth.bmp && \
-    sha1sum res_new/result_alt_smooth.bmp result_alt_smooth.bmp && \
+    sha1sum ../tests_res/result_RGB.bmp result_RGB.bmp && \
+    sha1sum ../tests_res/result_RGB_smooth.bmp result_RGB_smooth.bmp && \
+    sha1sum ../tests_res/result_alt_smooth.bmp result_alt_smooth.bmp && \
     rm -v *bmp
