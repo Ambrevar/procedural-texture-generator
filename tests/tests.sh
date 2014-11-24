3264d685c9c722e56b4c2d896a954d87445eda27
Pierre Neidhardt <ambrevar@gmail.com>
2014-11-24 12:07:44 +0100

    tests: Clarify output

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
-../src/procedural-textures ../data/wood.ptx && ls -l *bmp && \
-    sha1sum ../tests_res/result_RGB.bmp result_RGB.bmp && \
-    sha1sum ../tests_res/result_RGB_smooth.bmp result_RGB_smooth.bmp && \
-    sha1sum ../tests_res/result_alt_smooth.bmp result_alt_smooth.bmp && \
-    rm -v *bmp
+
+res="$(realpath "$0")"
+res="${res%/*}/res"
+
+root=..
+
+sumcheck() {
+	output=$(sha1sum "$1" "$2" | cut -d' ' -f1)
+	sum1=${output%
+*}
+	sum2=${output#*
+}
+	if [ "$sum1" = "$sum2" ]; then
+		echo "SUCCESS: ${1##*/} ${2##*/}"
+	else
+		echo "FAIL: ${1##*/} ${2##*/}"
+	fi
+}
+
+"$root"/src/procedural-textures "$root"/data/wood.ptx 2>/dev/null
+sumcheck "$res"/result_RGB.bmp result_RGB.bmp
+sumcheck "$res"/result_RGB_smooth.bmp result_RGB_smooth.bmp
+sumcheck "$res"/result_alt_smooth.bmp result_alt_smooth.bmp
+rm *bmp
